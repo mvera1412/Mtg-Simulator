f904bcaae9209c7b4c322cbd519b697480febea0
M Vera <vera_matias@hotmail.com>
2022-07-29 16:17:56 -0300

    tablas in plots

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from prettytable import PrettyTable
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+import time
+from copy import deepcopy
+import pandas as pd    
 
 def save_dict(dict):
   f = open("dict","w")
@@ -60,55 +62,66 @@ def card2image(card):
       return None
       
 class Match:
-  deck = []
-  hand = []
-  play = []
-  graveyard = []
-  file_name = ''
-  text = False
+  def __init__(self):  
+    self.deck = []
+    self.hand = []
+    self.play = []
+    self.graveyard = []
+    self.file_name = ''
+    self.text = False
+    self.previous_state = None
 
   def action(self,moving,idx=0):
     print(moving)
     if moving == 'restart':
+      self.previous_state = None  
       self = restart(self.file_name)  
     elif moving == 'draw':
+      self.previous_state = None   
       change(self.deck,self.hand,0)
-    elif moving == 'play':
+    elif moving == 'play': 
+      self.previous_state = deepcopy(self)  
       change(self.hand,self.play,idx)
     elif moving == 'discard':
+      self.previous_state = deepcopy(self)
       change(self.play,self.graveyard,idx)
     elif moving == 'mulligan':
+      self.previous_state = None  
       change(self.hand,self.deck,idx)
+    elif moving == 'undo':
+      if self.previous_state:
+          self = deepcopy(self.previous_state)
+      else:
+          print('No se puede deshacer')
+          time.sleep(0.5) 
     else:
       print('No te equivoques')
     return self
 
 
-  def show(self):
+  def show(self, T=False):
     os.system('clear') # Local
     if self.text:
       diff = len(self.hand)-len(self.play)
       aux_hand = self.hand
       aux_play = self.play
       m = max((len(self.hand),len(self.play)))
-      idxs = list(range(m))
       if diff>0:
         for _ in range(diff):
           aux_play = aux_play + [" "]
       elif diff<0:
         for _ in range(-diff):
           aux_hand = aux_hand + [" "]
-      l = [[idxs[k],aux_hand[k],aux_play[k]] for k in range(m)]
-      table = PrettyTable(['Idx', 'Hand', 'Play'])
-      for rec in l:
-        table.add_row(rec)
-      print(table)
+      l = [[aux_hand[k],aux_play[k]] for k in range(m)]
+      render_mpl_table(l, ['Hand','Play'], col_width=3.5, suptitle = T)
     else:
       nhand=len(self.hand)
       nplay=len(self.play)
       maximo = max(nhand,nplay)
       fig, axs = plt.subplots(2,maximo+1,figsize=(15, 6))
       if maximo > 0:
+          if T:
+              fig.suptitle(T,color='red')
           axs[1,0].text(0.3, 0.5, 'Hand')
           axs[1,0].axis('off')
           for idx, card in enumerate(self.hand):
@@ -175,6 +188,7 @@ def restart(file_name):
   
 def keep_mulligan():
   while 1:
+    time.sleep(1)  
     x = input('(K)eep or (M)ulligan? ')
     if x=='k':
       mulligan = False
@@ -199,23 +213,70 @@ def start(file_name,texto):
     match.file_name = file_name
     match = match.action('restart')
     match.text = texto
-    match.show()
+    match.show('(K)eep or (M)ulligan?')
     mulligan = keep_mulligan()  
     while mulligan:
       match = match.action('restart')
       match.text = texto
       n_mul +=1
-      match.show()
+      match.show('(K)eep or (M)ulligan?')
       if n_mul>6:
           mulligan = False
       else:
           mulligan = keep_mulligan()
     while n_mul+len(match.hand)>7:
+      match.show('Return a card')  
       match = return_card(match)
-      match.show()
     return match     
+
+def render_mpl_table(listado, titulos, col_width=3.0, row_height=0.625, font_size=14,
+                     header_color='#40466e', row_colors=['#f1f1f2', 'w'], edge_color='w',
+                     bbox=[0, 0, 1, 1], header_columns=0, suptitle = False):
+    newlist = deepcopy(listado)
+    newlist.insert(0,titulos)
+    data = pd.DataFrame(newlist)
+    size = (np.array(data.shape[::-1]) + np.array([0, 1])) * np.array([col_width, row_height])
+    fig, ax = plt.subplots(figsize=size)
+    if suptitle:
+        fig.suptitle(suptitle,color='red')
+    ax.axis('off')
+    indexs = list(range(len(data)-1))
+    indexs.insert(0,'')
+    mpl_table = ax.table(cellText=data.values, bbox=bbox, rowLabels=indexs)
+    mpl_table.auto_set_font_size(False)
+    mpl_table.set_fontsize(font_size)
+    for k, cell in mpl_table._cells.items():
+        cell.set_edgecolor(edge_color)
+        if k[0] == 0 or k[1] < header_columns:
+            cell.set_text_props(weight='bold', color='w')
+            cell.set_facecolor(header_color)
+        else:
+            cell.set_facecolor(row_colors[k[0]%len(row_colors) ])
+    plt.show() 
+
     
-    
+def elegir_archivo(listado):
+    lista = []
+    for rec in listado:
+        lista.append([rec])
+    os.system('clear') # Local
+    render_mpl_table(listado, 'Choose a file', col_width=2.5)
+    plt.show()
+    x = input('Choose a file ')
+    return x
+
+def elegir_interfaz(filename):
+    plt.figure(figsize=[0.1,0.1])
+    plt.text(0.0, 0.0,'(I)mages or (T)ext? ', fontsize = 50)
+    plt.axis('off')
+    plt.show()
+    x = input('(I)mages or (T)ext? ')
+    return (x[0]=='t') 
+
+def menu_principal():
+    x = input('(D)raw - (P)lay - (G)raveyard - (U)ndo - (R)estart - (C)hange deck - (Q)uit ')
+    return x
+
 if __name__ == '__main__':
 	file_loaded = False
 	quit = False
@@ -229,38 +290,32 @@ if __name__ == '__main__':
 	        arr2.append(arr[k])
 	    m = len(arr2)
 	    l = [[k,arr2[k]] for k in range(m)]
-	    table = PrettyTable(['Idx', 'File name'])
-	    for rec in l:
-	      table.add_row(rec)
-	    os.system('clear') # Local
-	    print(table)
-	    x = input('Choose a file ')
+	    x = elegir_archivo(arr2) #l
 	    if(x >= '0' and x < str(m)):
 	      file_name = arr2[int(x)]
 	      file_loaded = True 
-	  x = input('(I)mages or (T)ext? ')
-	  texto = (x[0]=='t')  
+	  texto = elegir_interfaz(file_name) 
 	  match = start(file_name,texto)
 	  playing = True
 	  while playing:
-	    x = input('(D)raw - (P)lay - (G)raveyard - (R)estart - (C)hange deck - (Q)uit ')
+	    match.show('(D)raw - (P)lay - (G)raveyard - (U)ndo - (R)estart - (C)hange deck - (Q)uit')
+	    x = menu_principal()
 	    if (x!='') and (x[0]=='d'):
-	      match = match.action('draw',match)
-	      match.show()
+	      match = match.action('draw')
 	    elif (x!='') and (x[0]=='p'):
 	      y = x[1:]
 	      if(y >= '0' and y < str(len(match.hand))):
 	        match = match.action('play',int(y))
-	        match.show()
 	      else:
 	        print('Which?')
 	    elif (x!='') and (x[0]=='g'):
 	      y = x[1:]
-	      if(y >= '0' and y < str(len(match.play))):
+	      if(y >= '0' and y < str(len(match.play))): 
 	        match = match.action('discard',int(y))
-	        match.show()
 	      else:
 	        print('Which?')
+	    elif (x!='') and (x[0]=='u'):
+	      match = match.action('undo')
 	    elif (x!='') and (x[0]=='r'):
 	      playing = False
 	    elif (x!='') and (x[0]=='q'):
